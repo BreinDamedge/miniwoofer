47526e70573922bf70424c4a757af9c46b322e76
GhostFire90 <evan.anderson_@outlook.com>
2026-09-03 12:02:46 -0700

    rebase onto main

--- indexing/index.go
+++
@@ -1,6 +0,0 @@
-// Package indexing implements the index data structures
-//
-// This file holds the implementation of the index interface
-package indexing
-
-// TODO: index interface definition
--- mhtml2html.go
+++
@@ -1,8 +0,0 @@
-package main
-
-/*
-* this will be implemented later once the rest of the systems have prototypes.
-* developing on linux machine rn which only has firefox, and i only have an
-* html archiving extention here so no way to get mhtml files without that
-* mhtml extention from chrome on the windows machine.
- */
