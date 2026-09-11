99a325399bea27998666c9301aee096abe6bed3d
GhostFire90 <evan.anderson_@outlook.com>
2026-09-11 11:59:52 -0700

    some infra changes

--- cmd/miniwoofer-tuning-evaluation/eval.go
+++ cmd/miniwoofer-tuning-evaluation/eval.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"io/fs"
 	"miniwoofer/impl"
 	"os"
--- flake.nix
+++ flake.nix
@@ -36,9 +36,9 @@
         packages.default = pkgs.buildGoModule {
           pname = "miniwoofer";
           version = "0.1.0";
-          src = ./.;
+          src = ./...;
 
-          vendorHash = "sha256-kXQ5b7pd+dRYItZmVFZAFWmzj0WVsvWAzOa6CKKaaLc=";
+          vendorHash = "sha256-eKzjSxK4/98yy6sUhzSHjren6p0CJauw9L7ixWqj4bM=";
         };
       }
     );
